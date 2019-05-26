daed907e207741779cab3628f7bb333c71607352
shaojason999 <shaojason999@gmail.com>
2019-05-26 10:10:33 +0000

    Complete All

--- problem_2.pl
+++ problem_2.pl
@@ -6,7 +6,7 @@ ancestor(A,B,ANS_NUM) :- A =:= B, assertz(ans(ANS_NUM,A)); parent(N,A), ancestor
 co_ancestor(A,B,ANS_NUM) :- ancestor(A,B,ANS_NUM); parent(N,B), co_ancestor(A,N,ANS_NUM).
 
 find(0) :- true.
-find(ANS_NUM) :- ANS_NUM > 0, write("Node 1: "), read(A), write("Node 2: "), read(B),not(co_ancestor(A,B,ANS_NUM)), assertz(ans(ANS_NUM,-1)), false; ANS_NUM1 is ANS_NUM-1, find(ANS_NUM1).
+find(ANS_NUM) :- ANS_NUM > 0, write("Node 1: "), read(A), write("Node 2: "), read(B), not(co_ancestor(A,B,ANS_NUM)), assertz(ans(ANS_NUM,-1)), false; ANS_NUM1 is ANS_NUM-1, find(ANS_NUM1).
 
 print(0) :- true.
 print(N) :- ans(N,X), write("LCA: "), write(X) ,nl, N1 is N-1, print(N1).
