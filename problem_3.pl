ba26964a1b7fac6ad3f9dd607f64f2baf4ec4662
shaojason999 <shaojason999@gmail.com>
2019-05-28 16:27:53 +0000

    Fixed: another circle soultion (last version is incorrect)

--- problem_3.pl
+++ problem_3.pl
@@ -1,13 +1,12 @@
-self_edge(0).
-self_edge(N) :- N>0, assertz(edge(N,N)), N1 is N-1, self_edge(N1).
-
 input_edge(0).
 input_edge(E) :- E>0, write('Node A: '), read(A), write('Node B: '), read(B), assertz(edge(A,B)), assertz(edge(B,A)), E1 is E-1, input_edge(E1).
 
-check_edge(A,B) :- edge(A,B); edge(A,C), C > A, check_edge(C,B).
-start_check_edge(A,B) :- check_edge(A,B), write('Yes'), nl; write('No'), nl.
+path(A,A,_).
+path(A,B,M) :- edge(A,B); edge(A,C), not(visited(C,M)), assertz(visited(C,M)), path(C,B,M).
+
+start_check_edge(A,B,M) :- assertz(visited(A,M)), path(A,B,M), write('Yes'), nl; write('No'), nl.
 
 queries(0).
-queries(M) :- M>0, write('Node A: '), read(A), write('Node B: '), read(B), start_check_edge(A,B), M1 is M-1, queries(M1).
+queries(M) :- M>0, write('Node A: '), read(A), write('Node B: '), read(B), start_check_edge(A,B,M), M1 is M-1, queries(M1).
 
-:- write('Nodes: '), read(N), self_edge(N), write('Edges: '), read(E), write('Set edges: '), nl, input_edge(E), write('Queries: '), read(M), queries(M), halt.
+:- write('Nodes: '), read(N), write('Edges: '), read(E), write('Set edges: '), nl, input_edge(E), write('Queries: '), read(M), queries(M), halt.
